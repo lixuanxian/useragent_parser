3355d31275d037ac64d091b79f4ec723e3130eb4
Jake Champion <me@jakechampion.name>
2020-01-13 15:53:50 +0000

    build: rewrite the update script to work with git subtree correctly

--- update.sh
+++ update.sh
@@ -7,16 +7,28 @@ set -o nounset
 
 npm ci;
 
-git remote add -f uap-ref-impl https://github.com/ua-parser/uap-ref-impl.git || true;
-git remote add -f uap-core https://github.com/ua-parser/uap-core.git || true;
+# create a working branch just to avoid messing up master if something goes wrong
+git checkout -B update-uap-core-regexes master
 
-git fetch uap-core master;
-git fetch uap-ref-impl master;
+# create a remote for the core repository and fetch from it
+git remote add -f uap-ref-impl-remote https://github.com/ua-parser/uap-ref-impl.git || true;
+git remote add -f uap-core-remote https://github.com/ua-parser/uap-core.git || true;
 
-git subtree pull --prefix uap-ref-impl uap-ref-impl master --squash;
-git subtree pull --prefix uap-core uap-core master --squash;
+# create a branch with the contents of the core repository's master branch
+git branch -f uap-ref-impl-branch uap-ref-impl-remote/master
+git branch -f uap-core-branch uap-core-remote/master
 
+# update the core subtree
+git merge --squash -s subtree --allow-unrelated-histories --no-commit uap-ref-impl-branch
+git merge --squash -s subtree --allow-unrelated-histories --no-commit uap-core-branch
+
+# commit the updates
 node ./compilers/vcl.js;
 node ./compilers/js.js;
+git add lib/
+git commit -m 'feat: update to the latest uap-core regex list'
 
-echo 'updating useragent_parser complete.';
+# push the commit to GitHub
+# git push
+
+echo 'updating complete.';
